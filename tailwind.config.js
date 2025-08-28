1ab92e9be680009aa4b154b315a2aab5d4b519f1
Satish Nikale <satishnikale7@gmail.com>
2025-08-28 23:02:13 +0530

    Add device size for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ export default {
         lightGray: "#f2f3f7",
         darkGray: "#6c6a6f",
       },
+      screens: {
+        sm: "640px", // small devices (mobile landscape / large mobiles)
+        md: "768px", // tablets
+        lg: "1024px", // laptops
+        xl: "1280px", // desktops
+        "2xl": "1536px", // large screens / TVs
+      },
     },
   },
   plugins: [],
