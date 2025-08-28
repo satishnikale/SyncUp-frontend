d073adf1e2e57d137edf7d5557fc774772dd796a
Satish Nikale <satishnikale7@gmail.com>
2025-08-29 00:29:32 +0530

    add Feature component

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
 import "../App.css";
+import { WhyChooseClearLink } from "../components/Features";
 import { Hero } from "../components/Hero";
 import { Navbar } from "../components/NavBar";
 export const LandingPage = () => {
   return <div className="w-screen h-auto">
     <Navbar />
     <Hero />
+    <WhyChooseClearLink />
   </div>;
 };
