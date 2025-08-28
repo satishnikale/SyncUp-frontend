0f516d738466ef70c6e21c692aa87e8358eef58d
Satish Nikale <satishnikale7@gmail.com>
2025-08-29 00:28:25 +0530

    change wrapper width as per devices

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -4,7 +4,7 @@ interface MainContentProps {
 // lg:w-8/12
 export const MainContent = ({ children }: MainContentProps) => {
     return (
-        <div className="w-11/12 max-w-[1080px] h-auto mx-auto">
+        <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8">
             {children}
         </div>
     );
