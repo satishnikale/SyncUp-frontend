6e34c551f4a69ae8c52e02f756a8d5915d286f74
Satish Nikale <satishnikale7@gmail.com>
2025-08-30 14:05:27 +0530

    add icnons

---
+++ src/icons/CheckCirlce.tsx
@@ -0,0 +1,10 @@
+export const CheckCircle = () => {
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-blue-600 flex-shrink-0">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+
+        </div>
+    )
+}
