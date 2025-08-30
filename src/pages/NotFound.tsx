205a12db5cffd7a248ef4f7e900c7475fdd387c2
Satish Nikale <satishnikale7@gmail.com>
2025-08-30 15:25:55 +0530

    add not found page error boundary

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export const NotFound = () => {
+    return (
+        <div className="w-screen h-screen flex flex-col gap-2 justify-center items-center text-2xl md:text-3xl lg:text-4xl font-medium font-inter">
+            <p>Sorry!</p>
+            <p>404 | Page Not Found</p>
+        </div>
+    )
+}
